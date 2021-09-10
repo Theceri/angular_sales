e94ddedae32d0e57b94d45153996c27ab277d103
Paul Theceri <paul.theceri@gmail.com>
2021-09-10 17:56:23 +0300

    basic app now working

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -1,33 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Sale } from './sales';
+import { SaleserviceService } from './saleservice.service';
 
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.css']
 })
+
 export class SalesComponent implements OnInit {
+  sales: Sale[] = [];
 
-  s1: Sale = {
-    id: 1,
-    name: "Condoms",
-    buyingPrice: 56,
-    sellingPrice: 76
-  };
-
-
-  s2: Sale = {
-    id: 2,
-    name: "Weed",
-    buyingPrice: 57,
-    sellingPrice: 77
-  };
-
-  salesArray: Sale[] = [this.s1, this.s2];
-
-  constructor() { }
+  constructor(public salesService: SaleserviceService) { }
 
   ngOnInit(): void {
+    this.sales = this.salesService.mySales;
   }
 
 }
--- src/app/sales/saleservice.service.ts
+++ src/app/sales/saleservice.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Sale } from './sales';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SaleserviceService {
 
-  constructor() { }
+export class SaleserviceService {
+  mySales: Sale[];
+
+  getService(): Sale[] {
+    //HTTP Request
+    return [
+      {
+        id: 1,
+        name: "Condoms",
+        buyingPrice: 56,
+        sellingPrice: 76
+      },
+      {
+        id: 2,
+        name: "Weed",
+        buyingPrice: 57,
+        sellingPrice: 77
+      }
+    ];
+  }
+
+  constructor() {
+    this.mySales = this.getService();
+  }
 }
